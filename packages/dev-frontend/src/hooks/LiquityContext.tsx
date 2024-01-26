5fe5d06c64573a0697bea69103970e68c0c891e2
Nikita Khromov <nikita1tup@gmail.com>
2024-01-26 17:44:13 +0200

    ad

--- packages/dev-frontend/src/hooks/LiquityContext.tsx
+++ packages/dev-frontend/src/hooks/LiquityContext.tsx
@@ -34,7 +34,7 @@ const wsParams = (network: string, infuraApiKey: string): [string, string] => [
 ];
 
 const projectNetworks: { [name: string]: Network } = {
-  autonity: { chainId: 65100000, name: "autonity" }
+  autonity: { chainId: 65100001, name: "autonity" }
 }
 
 const getNetwork = (network: Networkish): Network => {
